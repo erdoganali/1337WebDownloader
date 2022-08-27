a6d7d3ee37a716d31015ad60dd6ee59434dcae13
TCALERDOGAN <TCALERDOGAN@TC16798561>
2022-08-27 11:36:23 +0300

    Add project files.

---
+++ 1337WebDownloader.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Net;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
